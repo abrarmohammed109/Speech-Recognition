c924c498f7ca0aac0ff91ddcb346c1f4a2f2625d
abrarmohammed109 <moasib.abrar@gmail.com>
2021-08-25 14:45:42 -0400

    created flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        print("FORM DATA RECEIVED")
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
