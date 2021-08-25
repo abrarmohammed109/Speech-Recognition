34270bda8c1c5376d98d46ad50e8b188e5ea8266
abrarmohammed109 <moasib.abrar@gmail.com>
2021-08-25 15:15:22 -0400

    included and imported speech recognition modules/algorithm logic and printed to terminal

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+# from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -6,6 +8,22 @@ app = Flask(__name__)
 def index():
     if request.method=="POST":
         print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+        
+        file = request.files['file']
+        if file.filename == "":
+            return redirect(request.url)
+        
+        if file: 
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            text = recognizer.recognize_google(data,key=None)
+            print(text)
+        
     return render_template('index.html')
 
 if __name__ == "__main__":
